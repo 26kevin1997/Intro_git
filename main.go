0dbc834f1dcba5913100d40f3dffe8f02858ed7e
Kevin Orozco <kevin.orodriguez@alumnos.udg.mx>
2020-09-20 14:18:04 -0500

    Capture la edad e imprimi la edad

--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import "fmt"
 
 func main()  {
 	var a float64
+	var b float64
 	fmt.Println("Cuanto mide un lado del cuadrado: ")
 	fmt.Scanf("%f", &a)
 	fmt.Println("El area es: ",a*a)
 	fmt.Println("Fin del programa")
 	fmt.Println("Otra modificacion")
+	fmt.Println("Que edad tienes")
+	fmt.Scan(&b)
+	fmt.Println("La edad es: ", b)
 }
