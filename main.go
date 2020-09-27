52e8523b20f4221643e083fe4118c271c4b7a6fc
Kevin Orozco <kevin.orodriguez@alumnos.udg.mx>
2020-09-27 13:40:48 -0500

    Comente fin del programa

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main()  {
 	fmt.Println("Cuanto mide un lado del cuadrado: ")
 	fmt.Scanf("%f", &a)
 	fmt.Println("El area es: ",a*a)
-	fmt.Println("Fin del programa")
+	//fmt.Println("Fin del programa")
 	fmt.Println("Otra modificacion")
 	fmt.Println("Que edad tienes")
 	fmt.Scan(&b)
